7325a4f9e17c026c1f43965781c8a9c2cf9deca3
Angela <angelakgo20@gmail.com>
2023-10-31 16:00:46 -0300

    gitingnore and change README

--- pages/api/completion.ts
+++ pages/api/completion.ts
@@ -1,19 +1,15 @@
 import { Configuration, OpenAIApi } from "openai-edge";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
-// Create an OpenAI API client (that's edge friendly!)
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(config);
 
-// IMPORTANT! Set the runtime to edge
 export const runtime = "edge";
 
 export default async function POST(req: Request) {
-  // Extract the `prompt` from the body of the request
   const { prompt } = await req.json();
-  // Ask OpenAI for a streaming completion given the prompt
 
   const response = await openai.createCompletion({
     model: "text-davinci-003",
@@ -26,9 +22,7 @@ export default async function POST(req: Request) {
     presence_penalty: 1,
   });
 
-  // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
 
-  // Respond with the stream
   return new StreamingTextResponse(stream);
 }
